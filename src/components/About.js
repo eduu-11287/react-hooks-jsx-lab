707342d24186161e1804dfddc7f53c2d9dd7f214
Edwin <edmboche141@gmail.com>
2025-01-17 02:18:41 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id = "about">
+    <h2>About Me</h2>
+    <p>I made this code work</p>
+    <img src = {image} alt = "Imade this"></img>
+  </div>;
 }
 
 export default About;
